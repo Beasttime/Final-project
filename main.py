a4c74da73c00c86c0d5c8c87b237926592f54294
Beasttime <91510298+Beasttime@users.noreply.github.com>
2021-09-28 22:02:01 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+#####   LIBARARIES IMPORTED ####
+from tkinter import *
+import tkinter as tk
+# from Tkinter import *
+# import Tkinter
+from tkinter.messagebox import askyesno
+from tkinter.messagebox import *
+from tkinter import messagebox
+
+import sqlite3
+
+#####   LIBARARIES IMPORTED ####
+
+
+window1 = tk.Tk()
+window1.geometry("600x600")
+window1.title("ORDERING SYSTEM")
+window1.maxsize(600,600)
+window1.minsize(600,600)
+window1.configure()
+def cartt():
+    window1.destroy()
+    import cart
+def sb():
+    window1.destroy()
+    import sb
+def sa():
+    window1.destroy()
+    import sa
+def up():
+    window1.destroy()
+    import signupmain
+
+def QU():
+    window1.destroy()
+    import loginmain
+user = tk.StringVar()
+passw = tk.StringVar()
+# keys = tk.StringVar()
+
+main1=tk.Label(window1,text="MADE IN US | LIFEIME GUARANTEE | BEST PRODUCTS",font=("times new roman",16))
+main1.pack()
+main2=tk.Label(window1,text="LEATHER CRAFT",fg="red",font=("times new roman",16))
+main2.pack()
+# image1 = Image.open("NEW.png")
+# test = ImageTk.PhotoImage(image1)
+# label1 = tk.Label(image=test)
+# label1.image = test
+
+# # Position image
+# label1.pack()
+### USERID ##3
+# userid=tk.Label(window1,text="Username",font=("times new roman",16))
+# userid.place(x=50,y=150)
+# useride = tk.Entry(window1, textvariable=user,width=20,font=("times new roman",16))
+# useride.place(x=150,y=150)
+# ## PASSWORD ##
+# password=tk.Label(window1,text="Password",font=("times new roman",16))
+# password.place(x=50,y=210)
+# passworde = tk.Entry(window1, textvariable=passw,width=20,font=("times new roman",16),show="*")
+# passworde.place(x=150,y=210)
+### BUTTON ##
+
+
+
+main3=tk.Label(window1,text="MENU",font=("times new roman",16))
+main3.pack()
+subm1=tk.Button(window1,text="SIGNUP",bg="lightblue",font=("times new roman",13),command=up)
+subm1.pack()
+subm2=tk.Button(window1,text="ABOUT US",bg="lightblue",font=("times new roman",13))
+subm2.pack()
+subm3=tk.Button(window1,text="SHOPS-BAGS",bg="lightblue",font=("times new roman",13),command=sb)
+subm3.pack()
+subm4=tk.Button(window1,text="SHOP-ACCESSORIES",bg="lightblue",font=("times new roman",13),command=sa)
+subm4.pack()
+subm5=tk.Button(window1,text="SPECIAL-ITEMS",bg="lightblue",font=("times new roman",13),command=sa)
+subm5.pack()
+subm6=tk.Button(window1,text="DISCOUNTS",bg="lightblue",font=("times new roman",13),command=sa)
+subm6.pack()
+quitb=tk.Button(window1,text="LOGOUT",bg='lightblue',font=("times new roman",13),command=QU)
+quitb.place(x=490,y=150)
+cart=tk.Button(window1,text="cart",bg='lightblue',font=("times new roman",13),command=cartt)
+cart.place(x=500,y=190)
+
+window1.mainloop()
